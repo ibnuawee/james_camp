6b1125fd29804260506497903f6251ce7f9a7942
ibnuawee <ibnuarifin@students.unnes.ac.id>
2024-12-10 21:05:49 +0700

    Menambahkan CRUD kategori item

--- camping/urls.py
+++ camping/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path('item/add/', views.camping_item_create, name='camping_item_create'),
     path('item/<int:pk>/edit/', views.camping_item_update, name='camping_item_update'),
     path('item/<int:pk>/delete/', views.camping_item_delete, name='camping_item_delete'),
+
+    path('category', views.camping_category, name='category_list'),
     path('category/add/', views.category_create, name='category_create'),
+    path('category/<int:pk>/edit/', views.category_item_update, name='category_update'),
+    path('category/<int:pk>/delete/', views.category_delete, name='category_delete'),
 ]
--- camping/views.py
+++ camping/views.py
@@ -63,6 +63,12 @@ def camping_item_delete(request, pk):
         return JsonResponse({'success': True, 'message': 'Item deleted successfully'})
     return JsonResponse({'success': False, 'message': 'Invalid request'}, status=400)
 
+
+@login_required
+def camping_category(request):
+    categories = Category.objects.all()
+    return render(request, 'camping/category_item.html', {'categories': categories})
+
 # Tambah kategori
 @login_required
 @user_passes_test(is_staff_or_admin)
@@ -71,7 +77,30 @@ def category_create(request):
         form = CategoryForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('camping_item_list')
+            return redirect('category_list')
     else:
         form = CategoryForm()
-    return render(request, 'camping/category_form.html', {'form': form})
+    return render(request, 'camping/category_add.html', {'form': form})
+
+@login_required
+@user_passes_test(is_staff_or_admin)
+def category_item_update(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('category_list')
+    else:
+        form = CategoryForm(instance=category)
+    return render(request, 'camping/category_add.html', {'form': form})
+
+# Hapus alat camping
+@login_required
+@user_passes_test(is_staff_or_admin)
+def category_delete(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        category.delete()
+        return JsonResponse({'success': True, 'message': 'Item deleted successfully'})
+    return JsonResponse({'success': False, 'message': 'Invalid request'}, status=400)
